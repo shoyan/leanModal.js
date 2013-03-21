cdb122d69e25b9456a83e61c60df94d0974b29bf
Shohei Yamasaki <shyamasaki@gmail.com>
2013-03-22 00:32:25 +0900

    Fix indentation

--- jquery.leanModal.js
+++ jquery.leanModal.js
@@ -21,52 +21,52 @@
                 var o = options;
                
                 $(this).click(function(e) {
-              
-              	var modal_id = $(this).attr("href");
 
-				$("#lean_overlay").click(function() { 
-                     close_modal(modal_id);                    
-                });
-                
-                $(o.closeButton).click(function() { 
-                     close_modal(modal_id);                    
-                });
-                         	
-              	var modal_height = $(modal_id).outerHeight();
-        	  	var modal_width = $(modal_id).outerWidth();
+                    var modal_id = $(this).attr("href");
 
-        		$('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
+                    $("#lean_overlay").click(function() { 
+                        close_modal(modal_id);                    
+                    });
 
-        		$('#lean_overlay').fadeTo(200,o.overlay);
+                    $(o.closeButton).click(function() { 
+                        close_modal(modal_id);                    
+                    });
 
-        		$(modal_id).css({ 
-        		
-        			'display' : 'block',
-        			'position' : 'fixed',
-        			'opacity' : 0,
-        			'z-index': 11000,
-        			'left' : 50 + '%',
-        			'margin-left' : -(modal_width/2) + "px",
-        			'top' : o.top + "px"
-        		
-        		});
+                    var modal_height = $(modal_id).outerHeight();
+                    var modal_width = $(modal_id).outerWidth();
 
-        		$(modal_id).fadeTo(200,1);
+                    $('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
 
-                e.preventDefault();
-                		
+                    $('#lean_overlay').fadeTo(200,o.overlay);
+
+                    $(modal_id).css({ 
+
+                        'display' : 'block',
+                        'position' : 'fixed',
+                        'opacity' : 0,
+                        'z-index': 11000,
+                        'left' : 50 + '%',
+                        'margin-left' : -(modal_width/2) + "px",
+                        'top' : o.top + "px"
+
+                    });
+
+                    $(modal_id).fadeTo(200,1);
+
+                    e.preventDefault();
+
               	});
              
             });
 
-			function close_modal(modal_id){
+            function close_modal(modal_id){
 
-        		$("#lean_overlay").fadeOut(200);
+                $("#lean_overlay").fadeOut(200);
 
-        		$(modal_id).css({ 'display' : 'none' });
-			
-			}
-    
+                $(modal_id).css({ 'display' : 'none' });
+
+            }
+
         }
     });
      
